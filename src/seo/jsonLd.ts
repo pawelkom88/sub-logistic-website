232b65ffc37e6519ef8bf84aa2112599c49f08c7
paw <pawel.komorkiewicz@yahoo.co.uk>
2025-08-29 07:50:23 +0100

    update json data

--- src/seo/jsonLd.ts
+++ src/seo/jsonLd.ts
@@ -26,54 +26,52 @@ export default function jsonLDGenerator({
 }) {
   const s = siteDataByLang[lang] || siteDataByLang.en;
   if (type === "post") {
-    return `<script type="application/ld+json">
-      {
-        "@context": "https://schema.org",
-        "@type": "BlogPosting",
-        "mainEntityOfPage": {
-          "@type": "WebPage",
-          "@id": "${url}"
-        },
-        "headline": "${post.title}",
-        "description": "${post.description}",
-        "image": "${post.image?.src || image}",
-        "author": {
-          "@type": "Person",
-          "name": "${post.author}",
-          "url": "${slugify(post.author || "")}"
-        },
-        "datePublished": "${post.date}"
-      }
-    </script>`;
+    const data = {
+      "@context": "https://schema.org",
+      "@type": "BlogPosting",
+      mainEntityOfPage: {
+        "@type": "WebPage",
+        "@id": url,
+      },
+      headline: post?.title,
+      description: post?.description,
+      image: post?.image?.src || image,
+      author: post?.author
+        ? {
+            "@type": "Person",
+            name: post.author,
+            url: `${s.url}/author/${slugify(post.author)}`,
+          }
+        : undefined,
+      datePublished: post?.date,
+    } as const;
+    return `<script type="application/ld+json">${JSON.stringify(data)}</script>`;
   }
-  if (type === "website")
-    return `<script type="application/ld+json">
-      {
+  if (type === "website") {
+    const data = {
       "@context": "https://schema.org/",
       "@type": "WebSite",
-      "name": "${s.title}",
-        "url": "${s.url}",
-        "logo": "${s.url}/.netlify/images?url=_astro%2Fsub-logistics-logo.DeiqvkTr.webp&fm=webp&w=261&h=152",
-        "image": "${image}",
-        "description": "${s.description}",  
-        "address": {
-          "@type": "PostalAddress",
-          "addressLocality": "${s.streetAddress}",
-          "addressRegion": "${s.region}",
-          "addressCountry": "${s.country}",
-          "postalCode": "${s.postalCode}",
-          "addressCountryCode": "PL",
-          "city": "${s.city}",
-          "region": "${s.region}",
-          "streetAddress": "${s.streetAddress}"
-        },
-        "contactPoint": {
-          "@type": "ContactPoint",
-          "telephone": "${s.telephone}",
-          "contactType": "customer support",
-          "areaServed": "PL",
-          "availableLanguage": "en-GB, pl-PL, de-DE",
-        }
-      }
-    </script>`;
+      name: s.title,
+      url: s.url,
+      logo: `${s.url}/.netlify/images?url=_astro%2Fsub-logistics-logo.DeiqvkTr.webp&fm=webp&w=261&h=152`,
+      image: image,
+      description: s.description,
+      address: {
+        "@type": "PostalAddress",
+        addressLocality: s.city,
+        addressRegion: s.region,
+        addressCountry: s.country,
+        postalCode: s.postalCode,
+        streetAddress: s.streetAddress,
+      },
+      contactPoint: {
+        "@type": "ContactPoint",
+        telephone: s.telephone,
+        contactType: "customer support",
+        areaServed: "PL",
+        availableLanguage: ["en-GB", "pl-PL", "de-DE"],
+      },
+    } as const;
+    return `<script type=\"application/ld+json\">${JSON.stringify(data)}</script>`;
+  }
 }
